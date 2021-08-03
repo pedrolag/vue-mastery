9b26b9165fe9bf1840e6a1d60df16001a5ada83e
Pedro Goncalves <pedro.goncalves@unimestre.com>
2021-08-03 14:10:42 -0300

    adding and displaying more info about the produt + adding product into the user's cart

--- intro-to-vue-2/js/main.js
+++ intro-to-vue-2/js/main.js
@@ -1,8 +1,71 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'Socks',
-        image: './assets/vmSocks-green.jpg',
-        stock_status_code: 0
+        product: {
+            id: 0,
+            name: "Socks",
+            inStock: true,
+            variant: 0,
+            variants: [
+                {
+                    variantId: 2234,
+                    variantName: "green",
+                    variantColor: "#0f0",
+                    variantImage: "./assets/vmSocks-green.jpg"
+                },
+                {
+                    variantId: 2235,
+                    variantName: "blue",
+                    variantColor: "#00f",
+                    variantImage: "./assets/vmSocks-blue.jpg"
+                }
+            ],
+            sizes: ["P", "M", "G"],
+            details: [
+                "80% cotton",
+                "20% polyester",
+                "Gender-neutral"
+            ],
+            image: "./assets/vmSocks-green.jpg"
+        },
+        cart: {
+            items: []
+        }
+    },
+    methods: {
+
+        /**
+         * Changes the variant of the product
+         * depending on the id of the variant that 
+         * is selected by the user
+         * 
+         * @param {number} variantId - id of the selected variant
+         */
+        changeProductVariant(variantId) {
+            this.product.variant = variantId;
+            this.changeProductImageByVariant(this.product.variant);
+        },
+
+        /**
+         * Changes the image displayed at the page
+         * depending on the id of the product variant
+         * 
+         * @param {number} variantId - id of the selected variant
+         */
+        changeProductImageByVariant(variantId) {
+            this.product.image = this.product.variants.find(variant => {
+                return variantId === variant.variantId;
+            }).variantImage;
+        },
+
+        /**
+         * Adds the product into the user's cart item list
+         * 
+         * @param {Object} product - selected product
+         */
+        addProductToCart(product) {
+            this.cart.items.push(product);
+        }
+
     }
 });
