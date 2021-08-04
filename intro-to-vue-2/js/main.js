9656ad2119baec5bd45196692660f16ec9e8528a
Pedro Goncalves <pedro.goncalves@unimestre.com>
2021-08-04 18:10:30 -0300

    creating a list of reviews

--- intro-to-vue-2/js/main.js
+++ intro-to-vue-2/js/main.js
@@ -1,92 +1,100 @@
+Vue.config.devtools = true;
+
 Vue.component('product', {
     template: `
         <div class="product">
 
-            <div class="product__left-container">
+            <div class="product__top-container">
 
-                <div class="product__image-container">
-                    <img class="product__image" :src="product.image">
+                <div class="product__left-container">
+
+                    <div class="product__image-container">
+                        <img class="product__image" :src="product.image">
+                    </div>
+
+                </div>
+
+                <div class="product__right-container">
+
+                    <div class="product__information">
+
+                        <h1 class="product__name">{{ productTitle }}</h1>
+                        
+                        <p 
+                            v-if="!product.inStock"
+                            class="product__stock-status product__stock-status--out" 
+                        >Out of stock</p>
+                        <p 
+                            v-else-if="product.inStock"
+                            class="product__stock-status product__stock-status--in"
+                        >In stock</p>
+
+                        <div class="product__details">
+                            <h2>Details</h2>
+                            <ul>
+                                <li 
+                                    v-for="detail in product.details"
+                                    class="product__details-item"
+                                >{{ detail }}</li>
+                            </ul>
+                        </div>
+
+                        <div class="product__shipping">
+                            <h2>Shipping</h2>
+                            <p class="product__shipping-text">{{ productShipping }}</p>
+                        </div>
+
+                        <div class="product__sizes">
+                            <h2>Sizes</h2>
+                            <div class="product__sizes-list">
+                                <div
+                                    v-for="size in product.sizes"
+                                    class="product__sizes-item"
+                                    :class="{'product__sizes-item--selected': product.size === size}"
+                                    @click="changeProductSize(size)"
+                                >
+                                    <p class="product__sizes-item-text">{{ size }}</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="product__variants">
+                            <h2>Variants</h2>
+                            <div class="product__variants-list">
+                                <div
+                                    v-for="variant in product.variants"
+                                    @click="changeProductVariant(variant.variantId)"
+                                    class="product__variants-item"
+                                >
+                                    <div
+                                        class="product__variants-color"
+                                        :style="{backgroundColor: variant.variantColor}"
+                                    ></div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="product__actions">
+
+                        <button 
+                            class="button button--primary"
+                            :class="{'button--disabled': !product.inStock}"
+                            @click="addToCart"
+                            :disabled="!product.inStock"
+                        >Add to cart</button>
+
+                    </div>
+
                 </div>
 
             </div>
 
-            <div class="product__right-container">
-
-                <div class="product__information">
-
-                    <h1 class="product__name">{{ productTitle }}</h1>
-                    
-                    <p 
-                        v-if="!product.inStock"
-                        class="product__stock-status product__stock-status--out" 
-                    >Out of stock</p>
-                    <p 
-                        v-else-if="product.inStock"
-                        class="product__stock-status product__stock-status--in"
-                    >In stock</p>
-
-                    <div class="product__details">
-                        <h2>Details</h2>
-                        <ul>
-                            <li 
-                                v-for="detail in product.details"
-                                class="product__details-item"
-                            >{{ detail }}</li>
-                        </ul>
-                    </div>
-
-                    <div class="product__shipping">
-                        <h2>Shipping</h2>
-                        <p class="product__shipping-text">{{ productShipping }}</p>
-                    </div>
-
-                    <div class="product__sizes">
-                        <h2>Sizes</h2>
-                        <div class="product__sizes-list">
-                            <div
-                                v-for="size in product.sizes"
-                                class="product__sizes-item"
-                                :class="{'product__sizes-item--selected': product.size === size}"
-                                @click="changeProductSize(size)"
-                            >
-                                <p class="product__sizes-item-text">{{ size }}</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="product__variants">
-                        <h2>Variants</h2>
-                        <div class="product__variants-list">
-                            <div
-                                v-for="variant in product.variants"
-                                @click="changeProductVariant(variant.variantId)"
-                                class="product__variants-item"
-                            >
-                                <div
-                                    class="product__variants-color"
-                                    :style="{backgroundColor: variant.variantColor}"
-                                ></div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div class="product__actions">
-
-                    <button 
-                        class="button button--primary"
-                        :class="{'button--disabled': !product.inStock}"
-                        @click="addProductToCart(product)"
-                        :disabled="!product.inStock"
-                    >Add to cart</button>
-
-                    <button
-                        class="button button--secondary"
-                    >cart {{ cart.items.length > 0 ? "(" + cart.items.length + ")" : null }}</button>
-                
-                </div>
-
+            <div class="product__reviews">
+                <product-review
+                    @review-submitted="addReview"
+                ></product-review>
             </div>
 
         </div>
@@ -127,11 +135,10 @@ Vue.component('product', {
                     "20% polyester",
                     "Gender-neutral"
                 ],
-                image: "./assets/vmSocks-green.jpg"
+                image: "./assets/vmSocks-green.jpg",
+                reviews: []
             },
-            cart: {
-                items: []
-            }
+            
         }
     },
     computed: {
@@ -181,25 +188,113 @@ Vue.component('product', {
             this.product.size = size;
         },
 
-        /**
-         * Adds the product into the user's cart item list
-         * 
-         * @param {Object} product - selected product
-         */
-        addProductToCart(product) {
-            if (!product.inStock) {
-                alert(`The product "${product.name}" can not be added to your cart since it is out of stock.`);
-                return;
-            }
-            this.cart.items.push(product);
+        addToCart() {
+            this.$emit('add-to-cart', {...this.product});
+        },
+
+        addReview(productReview) {
+            this.product.reviews.push(productReview);
         }
 
+    }
+});
+
+Vue.component('product-review', {
+    template: `
+        <form @submit.prevent="submitReview">
+            <div class="product__review-input-field">
+                <label class="product__review-label" for="name">Name</label>
+                <input id="name" type="text" v-model="name">
+            </div>
+
+            <div class="product__review-input-field">
+                <label class="product__review-label">Rating</label>
+                <span class="product__review-radio">
+                    <input id="rating-1" type="radio" v-model.number="rating" value="1">
+                    <label for="rating-1">1</label>
+                </span>
+                
+                <span class="product__review-radio">
+                    <input id="rating-2" type="radio" v-model.number="rating" value="2">
+                    <label for="rating-2">2</label>
+                </span>
+
+                <span class="product__review-radio">
+                    <input id="rating-3" type="radio" v-model.number="rating" value="3">
+                    <label for="rating-3">3</label>
+                </span>
+
+                <span class="product__review-radio">
+                    <input id="rating-4" type="radio" v-model.number="rating" value="4">
+                    <label for="rating-4">4</label>
+                </span>
+
+                <span class="product__review-radio">
+                    <input id="rating-5" type="radio" v-model.number="rating" value="5">
+                    <label for="rating-5">5</label>
+                </span>
+            </div>
+            
+            <div class="product__review-input-field">
+                <label class="product__review-label" for="review">Review</label>
+                <textarea id="review" v-model="review"></textarea>
+            </div>
+
+            <button class="button button--primary" type="submit">Send Review</button>
+        </form>
+    `,
+    data() {
+        return {
+            name: "",
+            rating: 0,
+            review: ""
+        }
+    },
+    methods: {
+        submitReview() {
+            let productReview = {
+                name: this.name,
+                rating: this.rating,
+                review: this.review
+            }
+
+            this.$emit('review-submitted', productReview);
+            this.clearReviewFields();
+        },
+
+        clearReviewFields() {
+            this.name = null;
+            this.rating = null;
+            this.review = null;
+        }
     }
 });
 
 var app = new Vue({
     el: '#app',
     data: {
-        premiumUser: true
+        premiumUser: true,
+        cart: {
+            items: []
+        }
+    },
+    computed: {
+        cartLabel() {
+            return `cart ${this.cart.items.length > 0 ? "(" + this.cart.items.length + ")" : ""}`;
+        }
+    },
+    methods: {
+        /**
+         * Adds the product into the user's cart item list
+         * 
+         * @param {Object} product - selected product
+         */
+         addProductToCart(product) {
+            if (!product.inStock) {
+                alert(`The product "${product.name}" can not be added to your cart since it is out of stock.`);
+                return;
+            }
+            this.cart.items.push(product);
+        }
     }
 });
